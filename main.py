fddbbf467dad56b1099dec7ae9833c86a9e89f85
Jake Jaworski <jakejaworski28@gmail.com>
2022-04-27 17:18:05 -0700

    Added Maze Solution

---
+++ main.py
@@ -0,0 +1,20 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+    
+while right_is_clear():
+    turn_left()
+    turn_left()
+      
+while at_goal() == False:
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
